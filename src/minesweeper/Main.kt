76add53b04aafce68f95ed331dd24b38920d572c
Paul Terry <paul.terry84@gmail.com>
2021-02-10 20:35:00 +0000

    completed step 3, generated hints for each position on the game board

--- src/minesweeper/Main.kt
+++ src/minesweeper/Main.kt
@@ -10,15 +10,15 @@ fun main() {
     println(minesweeper.displayGameBoard())
 }
 
-
 class Minesweeper(
     private val mines: Int,
-    private val boardSize: Int = 9) {
+    boardSize: Int = 9) {
 
     private val gameBoard = Array(boardSize) { Array(boardSize) { '.' } }
 
     init {
         this.placeMinesOnBoard()
+        this.generateHints()
     }
 
     fun displayGameBoard(): String {
@@ -39,6 +39,34 @@ class Minesweeper(
             if (gameBoard[x][y] == '.') {
                 gameBoard[x][y] = 'X'
                 numOfPlacedMines++
+            }
+        }
+    }
+
+    private fun generateHints() {
+        var mineCounter = 0
+        for (y in gameBoard.indices) {
+            for (x in gameBoard[y].indices) {
+                // check the previous row only if the current row is > 0
+                if (y > 0) {
+                    if (x > 0 && gameBoard[y - 1][x - 1] == 'X') mineCounter++
+                    if (gameBoard[y - 1][x] == 'X') mineCounter++
+                    if (x < gameBoard[y].lastIndex && gameBoard[y - 1][x + 1] == 'X') mineCounter++
+                }
+
+                // check the current row
+                if (x > 0 && gameBoard[y][x - 1] == 'X') mineCounter++
+                if (x < gameBoard[y].lastIndex && gameBoard[y][x + 1] == 'X') mineCounter++
+
+                // check the next row only if the current is < gameBoard.lastIndex
+                if (y < gameBoard.lastIndex) {
+                    if (x > 0 && gameBoard[y + 1][x - 1] == 'X') mineCounter++
+                    if (gameBoard[y + 1][x] == 'X') mineCounter++
+                    if (x < gameBoard[y].lastIndex && gameBoard[y + 1][x + 1] == 'X') mineCounter++
+                }
+
+                if (gameBoard[y][x] == '.' && mineCounter > 0) gameBoard[y][x] = (mineCounter + '0'.toInt()).toChar()
+                mineCounter = 0
             }
         }
     }
